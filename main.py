06b22e4fd175413fbc3d810901311404880e583d
nicograzio <nicholasgrazioli@hotmail.it>
2026-07-14 16:01:29 +0200

    Add application entry point

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from ui.main_window import MainWindow
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.showFullScreen()
+    sys.exit(app.exec())
