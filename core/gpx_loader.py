1180dbaa4a741e8f5e8c93b51f50ab9c4ccf277f
nicograzio <nicholasgrazioli@hotmail.it>
2026-07-17 08:26:52 +0200

    Add GPX raw point and TrackPoint debug logging

--- core/gpx_loader.py
+++ core/gpx_loader.py
@@ -2,6 +2,8 @@ import gpxpy
 
 from core.track import Track, TrackPoint
 
+DEBUG_IMPORT = True
+
 
 def load_gpx(path):
     track = Track(path)
@@ -9,16 +11,29 @@ def load_gpx(path):
     with open(path, "r", encoding="utf-8") as file:
         gpx = gpxpy.parse(file)
 
+    index = 0
+
     for gpx_track in gpx.tracks:
         for segment in gpx_track.segments:
             for point in segment.points:
-                track.add_point(
-                    TrackPoint(
-                        latitude=point.latitude,
-                        longitude=point.longitude,
-                        altitude=point.elevation,
-                        timestamp=point.time,
-                    )
+                if DEBUG_IMPORT and index < 10:
+                    print("=== GPX RAW POINT", index, "===")
+                    print(vars(point))
+
+                track_point = TrackPoint(
+                    latitude=point.latitude,
+                    longitude=point.longitude,
+                    altitude=point.elevation,
+                    timestamp=point.time,
                 )
+
+                if DEBUG_IMPORT and index < 10:
+                    print("=== TRACK POINT", index, "===")
+                    print(vars(track_point))
+
+                track.add_point(track_point)
+                index += 1
+
+    print("GPX points loaded:", len(track.points))
 
     return track
