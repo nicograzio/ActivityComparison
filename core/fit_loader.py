1512aa4701dfd5bcf83ad5599bc87d01cc1f615d
nicograzio <nicholasgrazioli@hotmail.it>
2026-07-19 00:28:22 +0200

    Remove FIT import debug logging

--- core/fit_loader.py
+++ core/fit_loader.py
@@ -2,24 +2,17 @@ from fitparse import FitFile
 
 from core.track import Track, TrackPoint
 
-DEBUG_IMPORT = True
-
 
 def load_fit(path):
     track = Track(path)
 
     fit = FitFile(path)
 
-    for index, record in enumerate(fit.get_messages("record")):
+    for record in fit.get_messages("record"):
         data = {}
 
         for field in record:
             data[field.name] = field.value
-
-        if DEBUG_IMPORT and index < 10:
-            print("=== FIT RAW RECORD", index, "===")
-            for key, value in data.items():
-                print(key, ":", value)
 
         latitude = data.get("position_lat")
         longitude = data.get("position_long")
@@ -39,12 +32,6 @@ def load_fit(path):
             heart_rate=data.get("heart_rate"),
         )
 
-        if DEBUG_IMPORT and index < 10:
-            print("=== TRACK POINT", index, "===")
-            print(vars(point))
-
         track.add_point(point)
-
-    print("FIT points loaded:", len(track.points))
 
     return track
