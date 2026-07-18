52342bfd4e1b74d7a533ab12b6ee9fe3fb71a0cf
nicograzio <nicholasgrazioli@hotmail.it>
2026-07-19 01:15:47 +0200

    Switch speed color palette to grayscale

--- core/colorizer.py
+++ core/colorizer.py
@@ -3,16 +3,11 @@ from PyQt6.QtGui import QColor
 
 def value_to_color(value, minimum, maximum):
     if maximum <= minimum:
-        return QColor(255, 255, 0)
+        return QColor(160, 160, 160)
 
     ratio = (value - minimum) / (maximum - minimum)
     ratio = max(0.0, min(1.0, ratio))
 
-    if ratio < 0.5:
-        r = int(ratio * 2 * 255)
-        g = 255
-    else:
-        r = 255
-        g = int((1 - (ratio - 0.5) * 2) * 255)
-
-    return QColor(r, g, 0)
+    intensity = int(235 - ratio * 140)
+    intensity = max(60, min(235, intensity))
+    return QColor(intensity, intensity, intensity)
