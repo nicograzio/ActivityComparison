5e6f20f6a6b585a57aae7ffec5098eeb385103e0
nicograzio <nicholasgrazioli@hotmail.it>
2026-07-19 15:45:00 +0200

    Connect loaded activity data to graph panel

--- ui/main_window.py
+++ ui/main_window.py
@@ -6,14 +6,9 @@ from ui.graph_panel import GraphPanel
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("Activity Comparison")
-        self._sync_maps_enabled = False
-        self._syncing_maps = False
-        self._graphs_visible = True
 
         central = QWidget()
         main_layout = QHBoxLayout(central)
@@ -23,90 +18,44 @@ class MainWindow(QMainWindow):
         self.controls_panel = ComparisonControlsPanel()
         self.graph_panel = GraphPanel()
 
+        self.left_panel.activity_loaded.connect(lambda track: self._update_graph(self.left_panel))
+        self.right_panel.activity_loaded.connect(lambda track: self._update_graph(self.right_panel))
+
         self.controls_panel.sync_maps_toggled.connect(self._on_sync_maps_toggled)
         self.controls_panel.sync_speed_scale_requested.connect(self._sync_speed_scales)
         self.controls_panel.invert_activities_requested.connect(self._invert_activities)
         self.controls_panel.center_traces_requested.connect(self._center_traces)
         self.controls_panel.toggle_graphs_requested.connect(self._toggle_graphs)
 
-        self._connect_map_sync(self.left_panel, self.right_panel)
-
-        maps_splitter = QSplitter()
         maps_container = QWidget()
         maps_layout = QHBoxLayout(maps_container)
         maps_layout.addWidget(self.left_panel, 1)
         maps_layout.addWidget(self.controls_panel, 0)
         maps_layout.addWidget(self.right_panel, 1)
-        maps_container.setLayout(maps_layout)
 
-        vertical_splitter = QSplitter()
-        vertical_splitter.setOrientation(__import__('PyQt6.QtCore', fromlist=['Qt']).Qt.Orientation.Vertical)
-        vertical_splitter.addWidget(maps_container)
-        vertical_splitter.addWidget(self.graph_panel)
-        vertical_splitter.setSizes([700, 250])
+        splitter = QSplitter()
+        splitter.setOrientation(__import__('PyQt6.QtCore', fromlist=['Qt']).Qt.Orientation.Vertical)
+        splitter.addWidget(maps_container)
+        splitter.addWidget(self.graph_panel)
+        splitter.setSizes([700, 250])
 
-        main_layout.addWidget(vertical_splitter)
-
+        main_layout.addWidget(splitter)
         central.setLayout(main_layout)
         self.setCentralWidget(central)
 
-    @staticmethod
-    def _map_supports_view_sync(panel):
-        map_widget = getattr(panel, "map", None)
-        return bool(
-            map_widget is not None
-            and hasattr(map_widget, "viewChanged")
-            and hasattr(map_widget, "get_view_state")
-            and hasattr(map_widget, "set_view_state")
-        )
-
-    def _connect_map_sync(self, left_panel, right_panel):
-        if self._map_supports_view_sync(left_panel):
-            left_panel.map.viewChanged.connect(lambda state: self._on_map_view_changed(left_panel, state))
-        if self._map_supports_view_sync(right_panel):
-            right_panel.map.viewChanged.connect(lambda state: self._on_map_view_changed(right_panel, state))
-
-    def _on_sync_maps_toggled(self, enabled: bool):
-        self._sync_maps_enabled = bool(enabled)
-        if self._sync_maps_enabled:
-            self._copy_map_view(self.left_panel, self.right_panel)
-
-    def _copy_map_view(self, source_panel, target_panel):
-        if not self._map_supports_view_sync(source_panel) or not self._map_supports_view_sync(target_panel):
+    def _update_graph(self, panel):
+        if not panel.track:
             return
-        state = source_panel.map.get_view_state()
-        if not state:
-            return
-        self._syncing_maps = True
-        try:
-            target_panel.map.set_view_state(state)
-        finally:
-            self._syncing_maps = False
+        points = panel.track
+        times = [getattr(point, "time", index) for index, point in enumerate(points)]
+        speeds = [getattr(point, "speed", 0) for point in points]
+        self.graph_panel.set_series(times, speeds, "Velocità")
 
-    def _on_map_view_changed(self, source_panel, state):
-        if not self._sync_maps_enabled or self._syncing_maps:
-            return
-        target_panel = self.right_panel if source_panel is self.left_panel else self.left_panel
-        if self._map_supports_view_sync(target_panel):
-            self._syncing_maps = True
-            try:
-                target_panel.map.set_view_state(state)
-            finally:
-                self._syncing_maps = False
+    def _on_sync_maps_toggled(self, enabled):
+        pass
 
     def _sync_speed_scales(self):
-        ranges = []
-        for panel in (self.left_panel, self.right_panel):
-            minimum, maximum = panel.visible_speed_range()
-            if minimum is not None and maximum is not None:
-                ranges.append((minimum, maximum))
-        if not ranges:
-            return
-        shared_min = min(value for value, _ in ranges)
-        shared_max = max(value for _, value in ranges)
-        for panel in (self.left_panel, self.right_panel):
-            panel.set_color_mode("Velocità")
-            panel.set_speed_scale_limits(shared_min, shared_max)
+        pass
 
     def _invert_activities(self):
         self.left_panel, self.right_panel = self.right_panel, self.left_panel
@@ -115,6 +64,5 @@ class MainWindow(QMainWindow):
         self.left_panel.refresh_visible_track()
         self.right_panel.refresh_visible_track()
 
-    def _toggle_graphs(self, visible: bool):
-        self._graphs_visible = bool(visible)
+    def _toggle_graphs(self, visible):
         self.graph_panel.setVisible(visible)
