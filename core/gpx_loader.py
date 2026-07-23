86cc9a55effc65c70c77719e1bd89c130e60f59e
nicograzio <nicholasgrazioli@hotmail.it>
2026-07-23 11:01:07 +0200

    Add comprehensive docstrings and call flow documentation

--- core/gpx_loader.py
+++ core/gpx_loader.py
@@ -1,9 +1,30 @@
+"""GPX import helpers.
+
+This module turns a GPX file into the internal ``Track`` model used by the
+rest of the application.
+
+Called by:
+    - ``ui.track_panel.TrackPanel.import_file``
+"""
+
 import gpxpy
 
 from core.track import Track, TrackPoint
 
 
 def load_gpx(path):
+    """Load a GPX file and return a ``Track`` instance.
+
+    Called by:
+        - ``TrackPanel.import_file`` when the chosen file has a ``.gpx``
+          extension.
+
+    Args:
+        path: Path to the GPX file.
+
+    Returns:
+        Track: Parsed activity.
+    """
     track = Track(path)
 
     with open(path, "r", encoding="utf-8") as file:
