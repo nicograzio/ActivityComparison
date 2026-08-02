ca097f029ca2b473c3c46e9db59fb2660e20654a
nicograzio <nicholasgrazioli@hotmail.it>
2026-08-02 17:12:31 +0200

    deleted maplibre_bridge.py unsed file

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from ui.main_window import MainWindow
 
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
